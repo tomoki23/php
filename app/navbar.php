695d37d9d4073db142f16f59e1c9c8779469ef39
tomoki <tmdesign.0223@gmail.com>
2023-05-28 10:39:49 +0900

    ナビゲーション

---
+++ app/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a href="./index.php" class="navbar-brand">サークルサイト</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a href="#" class="nav-link">お知らせ</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">アルバム</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">掲示板</a></li>
+        </ul>
+    </div>
+</nav>
