b7faa3a2acba06da32ad576de889ef0f5ebb29c0
tomoki <tmdesign.0223@gmail.com>
2023-05-28 10:39:34 +0900

    tutorial メイン画面

---
+++ app/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>サークルサイト</title>
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+        <div>
+            <h1>サークルサイト</h1>
+        </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>
+        window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+
+</html>
