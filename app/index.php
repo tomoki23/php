3c242744148c974e75cc27db115c3d417bda6c32
tomoki <tmdesign.0223@gmail.com>
2023-05-28 11:10:27 +0900

    詳細ページ

--- app/index.php
+++ app/index.php
@@ -1,3 +1,6 @@
+<?php
+$fp = fopen("info.txt", "r");
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -14,9 +17,23 @@
 
     <main role="main" class="container" style="padding:60px 15px 0">
         <div>
-            <h1>サークルサイト</h1>
+            <h1>お知らせ</h1>
+            <?php
+            if ($fp) {
+                $title = fgets($fp);
+                if ($title) {
+                    echo '<p><a href="info.php">' . $title . '</a></p>';
+                } else {
+                    echo '<p>お知らせはありません。</p>';
+                }
+                fclose($fp);
+            } else {
+                echo '<p>お知らせはありません。</p>';
+            }
+            ?>
         </div>
     </main>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
     <script>
         window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')
