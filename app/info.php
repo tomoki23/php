a017cb556e1ef92be687a6eec6f97acf754c15ec
tomoki <tmdesign.0223@gmail.com>
2023-05-28 11:11:55 +0900

    詳細ページ

---
+++ app/info.php
@@ -0,0 +1,54 @@
+<?php
+$fp = fopen("info.txt", "r");
+$line = [];
+$body = "";
+if ($fp) {
+    while (!feof($fp)) {
+        $line[] = fgets($fp);
+    }
+    fclose($fp);
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>サークルサイト</title>
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+        <div>
+            <h1>お知らせ</h1>
+            <?php
+            if (count($line) > 0) {
+                for ($i = 0; $i < count($line); $i++) {
+                    if ($i == 0) {
+                        echo '<h2>' . $line[0] . '</h2>';
+                    } else {
+                        $body .= $line[$i] . '<br>';
+                    }
+                }
+            } else {
+                $body = 'お知らせはありません。';
+            }
+            echo '<p>' . $body . '</p>';
+            ?>
+        </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>
+        window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+
+</html>
